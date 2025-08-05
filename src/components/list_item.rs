cfad8ded1eccb6477800dda9da757e4394cbf35b
EllaCouch20 <ella@orange.me>
2025-08-05 12:21:00 +0200

    wip

--- src/components/list_item.rs
+++ src/components/list_item.rs
@@ -13,13 +13,13 @@ impl ListItemBitcoin {
     pub fn bitcoin(ctx: &mut Context, is_received: bool, btc: f64, price: f64, date: Timestamp, on_click: impl FnMut(&mut Context) + 'static) -> ListItem {
         let title = if is_received { "Received bitcoin" } else { "Sent bitcoin" };
         let usd = &format_usd(btc * price);
-        ListItem::new(ctx, true, title, None, Some(&date.friendly()), None, Some(usd), Some("Details"), None, None, None, on_click)
+        ListItem::new(ctx, true, title, None, Some(&date.friendly()), None, Some(usd), Some("Details"), None, None, None, true, on_click)
     }
 
     pub fn bitcoin_sending(ctx: &mut Context, btc: f64, price: f64, on_click: impl FnMut(&mut Context) + 'static) -> ListItem {
         let color = ctx.theme.colors.status.warning;
         let usd = &format_usd(btc * price);
         let btc = &format_nano_btc(btc);
-        ListItem::new(ctx, true, "Sending bitcoin", Some(("warning", color)), Some("unconfirmed"), None, Some(usd), Some(btc), None, None, None, on_click)
+        ListItem::new(ctx, true, "Sending bitcoin", Some(("warning", color)), Some("unconfirmed"), None, Some(usd), Some(btc), None, None, None, true, on_click)
     }
 }
